0dde06cd16413e29a5633f5a4cef47a2d1b50ac8
Dominik Visca <github@heliacal.xyz>
2023-10-16 19:26:24 +0200

    feat: massive performance boost toggling layer visibility

--- resources/components/useLayers.js
+++ resources/components/useLayers.js
@@ -6,32 +6,41 @@ export const useLayers = (layerConfigs, layerVisibility, setLayerVisibility) => 
 
     useEffect(() => {
         const loadLayers = async () => {
-            const loadedLayers = await Promise.all(
-                Object.values(layerConfigs).map(layerConfig => createLayer(layerConfig))
-            );
-            setLayers(loadedLayers);
+            try {
+                const loadedLayers = await Promise.all(
+                    Object.values(layerConfigs).map(layerConfig => createLayer(layerConfig))
+                );
+                setLayers(loadedLayers);
+            } catch (error) {
+                console.error("Failed to load layers:", error);
+            }
         };
 
         loadLayers();
     }, [layerConfigs]);
 
-    const handleToggleLayer = async (layerId) => {
-        const updatedLayers = await Promise.all(
-            layers.map(async layer => {
-                if (layer.id === layerId) {
-                    const originalConfig = layerConfigs[layerId];
-                    const updatedLayer = await createLayer({
-                        ...originalConfig,
-                        initialVisible: !layer.props.visible
-                    });
-                    return updatedLayer;
-                }
-                return layer;
-            })
-        );
+    const handleToggleLayer = (layerId) => {
+        const targetLayerIndex = layers.findIndex(layer => layer.id === layerId);
+        if (targetLayerIndex === -1) return;
+
+        const targetLayer = layers[targetLayerIndex];
+        const updatedLayer = targetLayer.clone({
+            visible: !targetLayer.props.visible
+        });
+
+        // Replace the target layer with the updated one
+        const updatedLayers = [
+            ...layers.slice(0, targetLayerIndex),
+            updatedLayer,
+            ...layers.slice(targetLayerIndex + 1)
+        ];
+
         setLayers(updatedLayers);
 
-        const updatedVisibility = { ...layerVisibility, [layerId]: !layerVisibility[layerId] };
+        const updatedVisibility = {
+            ...layerVisibility,
+            [layerId]: !layerVisibility[layerId]
+        };
         setLayerVisibility(updatedVisibility);
     };
 
