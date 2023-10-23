b5ce9772cf3baa864c50a472cb8e6f5adfc73cd9
Dominik Visca <github@heliacal.xyz>
2023-10-23 10:57:20 +0200

    feat: parse different gender and year of birth values during import

--- app/Imports/CitizensMasterImport.php
+++ app/Imports/CitizensMasterImport.php
@@ -37,23 +37,26 @@ class CitizensMasterImport implements ToModel, WithChunkReading, WithHeadingRow,
         }
 
         $geocodedAddress = $this->geocodeAddress(
-            $row[$this->columnMapping['zip_code']],
-            $row[$this->columnMapping['city']],
-            $row[$this->columnMapping['street']],
-            $row[$this->columnMapping['housenumber']],
-            $row[$this->columnMapping['housenumber_extra']]
+            $row[$this->columnMapping["zip_code"]],
+            $row[$this->columnMapping["city"]],
+            $row[$this->columnMapping["street"]],
+            $row[$this->columnMapping["housenumber"]],
+            $row[$this->columnMapping["housenumber_extra"]]
         );
 
+        $gender = $this->parseGender($row[$this->columnMapping["gender"]]);
+        $yearOfBirth = $this->parseYearOfBirth($row[$this->columnMapping["year_of_birth"]]);
+
         return new CitizensMaster([
-            'gender' => $row[$this->columnMapping['gender']],
-            'year_of_birth' => $row[$this->columnMapping['year_of_birth']],
-            'dataset_date' => $this->dataset_date,
-            'zip_code' => $row[$this->columnMapping['zip_code']],
-            'city' => $row[$this->columnMapping['city']],
-            'street' => $row[$this->columnMapping['street']],
-            'housenumber' => $row[$this->columnMapping['housenumber']],
-            'housenumber_ext' => $row[$this->columnMapping['housenumber_extra']],
-            'geometry'=> Point::make($geocodedAddress['lon'], $geocodedAddress['lat'])
+            "gender" => $gender,
+            "year_of_birth" => $yearOfBirth,
+            "dataset_date" => $this->dataset_date,
+            "zip_code" => $row[$this->columnMapping["zip_code"]],
+            "city" => $row[$this->columnMapping["city"]],
+            "street" => $row[$this->columnMapping["street"]],
+            "housenumber" => $row[$this->columnMapping["housenumber"]],
+            "housenumber_ext" => $row[$this->columnMapping["housenumber_extra"]],
+            "geometry"=> Point::make($geocodedAddress["lon"], $geocodedAddress["lat"])
         ]);
     }
 
@@ -65,8 +68,42 @@ class CitizensMasterImport implements ToModel, WithChunkReading, WithHeadingRow,
     public function getCsvSettings(): array
     {
         return [
-            'input_encoding' => 'ISO-8859-1'
+            "input_encoding" => "ISO-8859-1"
         ];
+    }
+
+    protected function parseGender(string $rawGender): string
+    {
+        $genderMap = [
+            "male" => "m",
+            "man" => "m",
+            "männlich" => "m",
+            "maennlich" => "m",
+            "mann" => "m",
+            "m" => "m",
+            "female" => "w",
+            "woman" => "w",
+            "weiblich" => "w",
+            "frau" => "w",
+            "w" => "w",
+            "diverse" => "d",
+            "divers" => "d",
+            "d" => "d"
+        ];
+
+        $normalizedGender = strtolower(trim($rawGender));
+
+        return $genderMap[$normalizedGender] ?? "d"; // Default to 'd' if unknown gender
+    }
+
+    protected function parseYearOfBirth($rawDate): int
+    {
+        if (is_numeric($rawDate)) {
+            return intval($rawDate); // Return if it's already a year
+        }
+
+        $date = new \DateTime($rawDate);
+        return $date->format('Y'); // Extract year from the date
     }
 
     protected function geocodeAddress($zipCode, $city, $street, $houseNumber, $houseNumberExtra)
@@ -86,11 +123,11 @@ class CitizensMasterImport implements ToModel, WithChunkReading, WithHeadingRow,
 
                 $data = json_decode($response, true);
 
-                if (isset($data[0]['lat'])) {
-                    Log::info('Successful geocode address: ' . $address);
+                if (isset($data[0]["lat"])) {
+                    Log::info("Successful geocode address: " . $address);
                     return $data[0];
                 } else {
-                    Log::warning('Failed to geocode address: ' . $address);
+                    Log::warning("Failed to geocode address: " . $address);
                 }
 
             } catch (\Exception $e) {
@@ -103,14 +140,14 @@ class CitizensMasterImport implements ToModel, WithChunkReading, WithHeadingRow,
 
         // After all retries failed, return default coordinates
         Log::error("All geocoding attempts failed for address: $address");
-        return ['lat' => 0, 'lon' => 0];
+        return ["lat" => 0, "lon" => 0];
     }
 
     protected function validateRow(array $row): bool
     {
         // Define all the columns, where values are not required
         $nullableColumns = [
-            $this->columnMapping['housenumber_extra']
+            $this->columnMapping["housenumber_extra"]
         ];
 
         // Check for missing columns
@@ -129,49 +166,49 @@ class CitizensMasterImport implements ToModel, WithChunkReading, WithHeadingRow,
 
         // Check for invalid data types or values
         // Ensure 'gender' is a string
-        if (!is_string($row[$this->columnMapping['gender']])) {
+        if (!is_string($row[$this->columnMapping["gender"]])) {
             Log::warning("Invalid data type for gender in row: ", $row);
             return false;
         }
 
         // Year of birth check
-        if (!is_numeric($row[$this->columnMapping['year_of_birth']]) ||
-            $row[$this->columnMapping['year_of_birth']] < 1900 ||
-            $row[$this->columnMapping['year_of_birth']] > date("Y")) {
+        if (!is_numeric($row[$this->columnMapping["year_of_birth"]]) ||
+            $row[$this->columnMapping["year_of_birth"]] < 1900 ||
+            $row[$this->columnMapping["year_of_birth"]] > date("Y")) {
             Log::warning("Invalid year_of_birth in row: ", $row);
             return false;
         }
 
         // ZIP code check (assuming alphanumeric ZIP, adjust as needed)
-        if (!preg_match('/^[a-zA-Z0-9]+$/', $row[$this->columnMapping['zip_code']])) {
+        if (!preg_match("/^[a-zA-Z0-9]+$/", $row[$this->columnMapping["zip_code"]])) {
             Log::warning("Invalid zip_code in row: ", $row);
             return false;
         }
 
         // City check
-        if (!is_string($row[$this->columnMapping['city']]) ||
-            strlen($row[$this->columnMapping['city']]) > 255) {
+        if (!is_string($row[$this->columnMapping["city"]]) ||
+            strlen($row[$this->columnMapping["city"]]) > 255) {
             Log::warning("Invalid city in row: ", $row);
             return false;
         }
 
         // Street check
-        if (!is_string($row[$this->columnMapping['street']]) ||
-            strlen($row[$this->columnMapping['street']]) > 255) {
+        if (!is_string($row[$this->columnMapping["street"]]) ||
+            strlen($row[$this->columnMapping["street"]]) > 255) {
             Log::warning("Invalid street in row: ", $row);
             return false;
         }
 
         // Housenumber check (accepting numeric, letters, and some special characters)
-        $housenumber = (string) $row[$this->columnMapping['housenumber']];
-        if (!preg_match('/^[a-zA-Z0-9\-]+$/', $housenumber)) {
+        $housenumber = (string) $row[$this->columnMapping["housenumber"]];
+        if (!preg_match("/^[a-zA-Z0-9\-]+$/", $housenumber)) {
             Log::warning("Invalid housenumber in row: ", $row);
             return false;
         }
 
         // Housenumber extension check
-        if (!is_null($row[$this->columnMapping['housenumber_extra']]) &&
-            !is_string($row[$this->columnMapping['housenumber_extra']])) {
+        if (!is_null($row[$this->columnMapping["housenumber_extra"]]) &&
+            !is_string($row[$this->columnMapping["housenumber_extra"]])) {
             Log::warning("Invalid housenumber_ext in row: ", $row);
             return false;
         }
